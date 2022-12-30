0886dc3d4ebc387e10c65830ba39bb70f9ed47dc
Rajarshi Ray <rayrajarshi9@gmail.com>
2022-12-30 23:05:18 +0530

    REACT-ROUTER FIX - V-6

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import NewsBase from './components/NewsBase';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export class App extends Component {
   render() {
     return (
       <div>
         <Router>
           <Navbar />
-          <Switch>
-            <Route exact path="/"><NewsBase key="1" country="in" pageSize={6} category="general" /></Route>
-            <Route exact path="/business"><NewsBase key="2" country="in" pageSize={6} category="business"/></Route>
-            <Route exact path="/entertainment"><NewsBase key="3" country="in" pageSize={6} category="entertainment" /></Route>
-            <Route exact path="/general"><NewsBase key="4" country="in" pageSize={6} category="general" /></Route>
-            <Route exact path="/health"><NewsBase key="5" country="in" pageSize={6} category="health" /></Route>
-            <Route exact path="/science"><NewsBase key="6" country="in" pageSize={6} category="science" /></Route>
-            <Route exact path="/sports"><NewsBase key="7" country="in" pageSize={6} category="sports" /></Route>
-            <Route exact path="/technology"><NewsBase key="8" country="in" pageSize={6} category="technology" /></Route>
-          </Switch>
+          <Routes>
+            <Route exact path="/"  element={<NewsBase country="in" key="general" pageSize={6} category="general" />}/>
+            <Route exact path="/business"  element={<NewsBase country="in" key="business" pageSize={6} category="business" />}/>
+            <Route exact path="/entertainment"  element={<NewsBase country="in" key="entertainment" pageSize={6} category="entertainment" />}/>
+            <Route exact path="/general"  element={<NewsBase country="in" key="general" pageSize={6} category="general" />}/>
+            <Route exact path="/health"  element={<NewsBase country="in" key="health" pageSize={6} category="health" />}/>
+            <Route exact path="/science"  element={<NewsBase country="in" key="science" pageSize={6} category="science" />}/>
+            <Route exact path="/sports"  element={<NewsBase country="in" key="sports" pageSize={6} category="sports" />}/>
+            <Route exact path="/technology"  element={<NewsBase country="in" key="technology" pageSize={6} category="technology" />}/>
+
+          </Routes>
         </Router>
       </div>
     )
